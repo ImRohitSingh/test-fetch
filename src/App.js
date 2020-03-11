715f455582da01da55e7f92420348d1762748c26
Rohit Singh <c.ronnie.r@gmail.com>
2020-03-11 15:23:42 +0530

    Removing extra loc and modifying member names

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  var x = getPersonPromise();
+function application() {
+  var promisePerson = getPersonPromise();
 
-  x.then(function(value) {
+  promisePerson.then(function(value) {
     var person = "name=" + value.name + ":id=" + value.id + ";";
     var spanElem = document.createElement("span");
     var txtElem = document.createTextNode(person);
@@ -18,17 +18,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div id="hi">Hi, </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
       </header>
     </div>
   );
@@ -45,4 +36,4 @@ async function getPersonPromise() {
   }
 }
 
-export default App;
+export default application;
