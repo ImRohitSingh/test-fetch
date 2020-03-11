47ec12dee1dd79e4a26c70fcbd51d7c0402364ba
Rohit Singh <c.ronnie.r@gmail.com>
2020-03-11 11:43:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var x = getPersonPromise();
+
+  x.then(function(value) {
+    var person = "name=" + value.name + ":id=" + value.id + ";";
+    var spanElem = document.createElement("span");
+    var txtElem = document.createTextNode(person);
+    spanElem.appendChild(txtElem);
+    document.getElementById("hi").appendChild(spanElem);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div id="hi">Hi, </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,6 +32,17 @@ function App() {
       </header>
     </div>
   );
+}
+
+async function getPersonPromise() {
+  try {
+    const response = await fetch('http://localhost:8000/rohit singh/012345');
+    const responseJson = await response.json();
+    return responseJson;
+  }
+  catch (error) {
+    console.error(error);
+  }
 }
 
 export default App;
